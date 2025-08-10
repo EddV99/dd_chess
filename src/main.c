e5c6b2e07d58f8fa185cabfcf9425d159d00c8d4
Eduardo Valdivia <eddval0808@gmail.com>
2025-08-09 20:48:45 -0600

    basic pseudo random generator

--- src/main.c
+++ src/main.c
@@ -2,11 +2,18 @@
 #include "board.h"
 #include "common.h"
 #include "move_generator.h"
+#include "prng.h"
 
 #include <stdio.h>
 
 int main() {
   printf("dd_chess\n");
+
+  prng_t prng = create_random_generator();
+
+  for (int i = 0; i < 100; i++) {
+    printf("%d. %u\n", i, get_random_number(&prng));
+  }
 
   return 0;
 }
