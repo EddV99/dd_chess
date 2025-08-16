c2f204f40005e418d9c1dd8af16f20f162760c26
Eduardo Valdivia <eddval0808@gmail.com>
2025-08-15 19:45:23 -0600

    perft debug function

---
+++ src/include/perft.h
@@ -0,0 +1,10 @@
+#ifndef DD_PERFT_H_
+#define DD_PERFT_H_
+
+#include "board.h"
+
+#include <stdint.h>
+
+uint64_t perft(int depth, board_t *board);
+
+#endif
---
+++ src/perft.c
@@ -0,0 +1,23 @@
+#include "perft.h"
+
+#include "board.h"
+#include "move.h"
+#include "move_generator.h"
+
+uint64_t perft(int depth, board_t *board) {
+  uint64_t nodes = 0;
+
+  if (depth == 0)
+    return 1ULL;
+
+  move_t move_list[MAX_MOVES] = {0};
+  int count = generate_pseudo_legal_moves(board, move_list);
+  for (int i = 0; i < count; i++) {
+    move_t move = move_list[i];
+    make_move(board, move);
+    // if (!IsIncheck()) TODO
+    nodes += perft(depth - 1, board);
+    unmake_move(board, move);
+  }
+  return nodes;
+}
